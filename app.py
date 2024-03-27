b82305ad1e7081f42e39f985e198f1023cc96ba5
Jadetawat <71974322+Jadetawat@users.noreply.github.com>
2024-03-27 11:55:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,27 @@
-from flask import Flask, render_template, url_for, request,redirect,send_from_directory
+from flask import Flask,render_template,url_for,request,redirect,send_from_directory
 from werkzeug.utils import secure_filename
 import os
 from script import pdf2img, png2jpg, information_extract
 from PIL import Image
 import pandas as pd
-import secrets
 import tempfile
 
 app = Flask(__name__)
-key = secrets.token_hex(16)
-app.secret_key = key
-
-
+user_dir=tempfile.mkdtemp()
+process_dir=tempfile.mkdtemp()
+output_dir=tempfile.mkdtemp()
 ALLOWED_EXTENSIONS = set(['pdf','png','jpg','jpeg'])
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
-# Modify the upload function to create a private directory for each user
-user_dir=tempfile.mkdtemp()
-process_dir=tempfile.mkdtemp()
-output_dir=tempfile.mkdtemp()
 @app.route('/', methods=['GET', 'POST'])
 def upload():
-
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            # Generate unique directory names for each user session
             save_location = os.path.join( user_dir, filename)
             input_process = os.path.join( process_dir, filename)
             file.save(save_location)
@@ -41,33 +32,28 @@ def upload():
             Json_Table_Path=os.path.join( process_dir, "table.json")
 
             if filename.lower().endswith(('.pdf')):
-                pdf2img(save_location, filename.split('.')[0])
                 filename = filename.split('.')[0] + '.jpg'
-                input_process = os.path.join('process', process_dir, filename)
+                input_process = os.path.join(process_dir, filename)
+                pdf2img(save_location, input_process)
+
             elif filename.lower().endswith(('.png')):
-                png2jpg(save_location, filename.split('.')[0])
                 filename = filename.split('.')[0] + '.jpg'
-                input_process = os.path.join('process', process_dir, filename)
+                input_process = os.path.join(process_dir, filename)
+                png2jpg(save_location, input_process)
+
             else:
-                with Image.open(save_location) as image:
-                    image.save(input_process, 'JPEG')
+                image=Image.open(save_location,'r')
+                image.save(input_process, 'JPEG')
 
-            with Image.open(input_process) as im:
-                information_extract(request.form['format'],im,cropped_table_path,removed_table_path,csv_output,json_output,Json_Table_Path)
-                
- 
+            im = Image.open(input_process,'r')
+
+            information_extract(request.form['format'],im,cropped_table_path,removed_table_path,csv_output,json_output,Json_Table_Path)
             
-            # Store the user_dir, process_dir, and output_dir in the session
-
             return redirect(url_for('download'))
-
     return render_template('index.html')
 
-
-# Modify the download function to display only files from the user's directory
 @app.route('/download')
 def download():
-
     files = os.listdir(output_dir)
     df = pd.DataFrame()
     try:
@@ -78,10 +64,7 @@ def download():
 
 @app.route('/download/<filename>')
 def download_file(filename):
-
         return send_from_directory(directory=output_dir, filename=filename, as_attachment=True)
-
-    
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0", port=5000)
